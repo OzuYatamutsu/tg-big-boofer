01df5ed48f30362a3bb8f7e2efc4008f1ebe7d6e
Sean Collins <secollins@linkedin.com>
2019-10-27 22:50:19 -0400

    Start integrating whitelist functionality

---
+++ database/schema.sql
@@ -0,0 +1,4 @@
+-- The following is a SQLite schema.
+CREATE TABLE IF NOT EXISTS whitelist (
+    user_id NUMERIC 
+)
--- handlers/group_handler.go
+++ handlers/group_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+
 	telegram "gopkg.in/tucnak/telebot.v2"
 )
 
@@ -10,4 +12,13 @@ func OnAddedToGroupHandler(bot *telegram.Bot, message *telegram.Message) {
 	bot.Send(message.Chat, "Hello! It's a warp gate!")
 
 	// TODO: Add all users in the chat to whitelist
+}
+
+// OnUserJoined handles what should happen when
+// the bot sees a new user join a group it is a part of.
+func OnUserJoined(bot *telegram.Bot, message *telegram.Message) {
+	welcomeMessage := fmt.Sprintf("Hello, @%v!", message.UserJoined.Username)
+	bot.Send(message.Chat, welcomeMessage)
+
+	// TODO: Add user to whitelist
 }
